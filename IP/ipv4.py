590924c196c4d0d129bb4e1b71ce88192aac3c97
sflaviu <sflaviu96@gmail.com>
2018-01-07 01:44:19 +0200

    Update ipv4.py

--- IP/ipv4.py
+++ IP/ipv4.py
@@ -22,15 +22,17 @@ class IPv4:
         secondI = random.randint(0, 255)
 
         currentAIp = '169.254.' + str(firstI) + '.' + str(secondI)
-
+        
+        port=2222;
         for ip in IPv4.allIps.items():
             if ip[1] != self.realIp:
-                conn = rpyc.connect(ip[1], 2222,
+                conn = rpyc.connect(ip[1], port,
                                     config={'allow_all_attrs': True})
                 answer = conn.root.check_ip(currentAIp)
                 if answer == True:
                     reconfiguredIP = reconfigure()
                     return reconfiguredIP
+            port=port+1
         IPv4.allIps[currentAIp] = self.realIp
         return currentAIp
 
