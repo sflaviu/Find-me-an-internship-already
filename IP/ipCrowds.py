67634cefd48686140052e3a83d9381e383a800e4
sflaviu <sflaviu96@gmail.com>
2018-01-06 22:27:53 +0200

    Add files via upload

---
+++ IP/ipCrowds.py
@@ -0,0 +1,218 @@
+import rpyc
+import subprocess
+import socket
+import thread
+import os
+import time
+from cmd import Cmd
+from rpyc.utils.server import ThreadedServer
+from rpDBMethods import Client
+from rpDBMethods import Internship
+from rpDBMethods import Location
+from rpDBMethods import Language
+from rpDBMethods import Company
+
+global data
+
+class PersistentData():
+    def __init__(self):
+        self.clients = []
+        #self.clientsRPYC = []
+        self.servers = []
+        # to be changed accordingly
+        self.host = "10.142.0.3"
+        self.port = 1024
+        self.dbHost = "10.142.0.2"
+        self.dbPort = 1234
+
+class CrowdsConsole(Cmd):
+    def do_stable_matching(self, args):
+        global data
+        ports = 1024
+        for i in data.clients:
+            ports += 1
+            thread.start_new_thread(
+                    os.system,
+                    ("python "+ "Server.py " + str(data.host) + " " + str(ports) + " " + str(data.dbHost) + " " + str(data.dbPort) + " " + str(data.host) + " " + str(data.port),))
+            # get port of each server in a list
+            data.servers.append((data.host, ports))
+        time.sleep(1)
+        for c in data.clients:
+            # name should be adjusted accordingly
+            conn = rpyc.connect(c[0], c[1], config={"allow_all_attrs": True})
+            s = conn.root
+            s.get_servers(data.servers)
+            s.choose_server()
+            clients = list(data.clients)
+            clients.remove(c)
+            s.get_clients(clients)
+            print "ok"
+            conn.close()
+
+    def do_show_conn(self, args):
+        global data
+        print str(data.clients)
+
+    def do_add_internship(self, args):
+        connection = rpyc.connect(data.dbHost, data.dbPort, config={"allow_all_attrs": True})
+        db = connection.root.DBConnection()
+        internships = db.getInternships()
+        languages = db.getLanguages()
+        locations = db.getLocations()
+        companies = db.getCompanies()
+        internship_id = 0
+        for internship in internships:
+            if internship.id > internship_id:
+                internship_id = internship.id
+        internship_id = internship_id + 1
+        print("Select one of the companies' IDs")
+        ids = []
+        for c in companies:
+            ids.append(c.id)
+            print(str(c.id) + " " + c.name)
+        company = -1
+        while (company == -1):
+            company_string = input();
+            try:
+                company = int(company_string)
+                if company not in ids:
+                    company = -1
+            except ValueError:
+                company = -1
+            if (company == -1):
+                print ("Invalid input, try again")
+        print("Select one of the languages' IDs")
+        ids = []
+        for c in languages:
+            ids.append(c.id)
+            print(str(c.id) + " " + c.name)
+        language = -1
+        while (language == -1):
+            language_string = input();
+            try:
+                language = int(language_string)
+                if language not in ids:
+                    language = -1
+            except ValueError:
+                language = -1
+            if (language == -1):
+                print ("Invalid input, try again")
+        print("Select one of the locations' IDs")
+        ids = []
+        for c in locations:
+            ids.append(c.id)
+            print(str(c.id) + " " + c.city)
+        location = -1
+        while (location == -1):
+            location_string = input();
+            try:
+                location = int(location_string)
+                if location not in ids:
+                    location = -1
+            except ValueError:
+                location = -1
+            if (location == -1):
+                print ("Invalid input, try again")
+        duration_string =input("Please insert the duration of the internship")
+        duration = -1
+        while (duration == -1):
+            try:
+                duration = int(duration_string)
+            except ValueError:
+                duration = -1
+                print ("Invalid input, try again")
+        experience_string = input("Please insert your experience")
+        experience = -1
+        while (experience == -1):
+            try:
+                experience = int(experience_string)
+            except ValueError:
+                experience = -1
+                print ("Invalid input, try again")
+        myInternship = Internship();
+        myInternship.id = internship_id;
+        myInternship.experience = experience;
+        myInternship.duration = duration;
+        myInternship.company = company;
+        myInternship.language = language;
+        myInternship.location =location
+        db.insertInternship(myInternship)
+        connection.close()
+
+    def do_remove_internship(self, args):
+        connection = rpyc.connect(data.dbHost, data.dbPort, config={"allow_all_attrs": True})
+        db = connection.root.DBConnection()
+        internships = db.getInternships()
+        print ("Select the id of the internship to delete")
+        ids = []
+        for i in internships:
+            ids.append(i.id)
+            print(str(i.id)+ " " +str(i.company))
+        id_string = input()
+        id = -1
+        while (id == -1):
+            try:
+                id = int(id_string)
+                if id not in ids:
+                    id = -1
+            except ValueError:
+                id = -1
+                print("Invalid input, please try again")
+        myInternship = Internship();
+        myInternship.id = id
+        db.deleteInternship(myInternship)
+
+    def do_quit(self, args):
+        print "Program terminated."
+        raise SystemExit
+
+
+class CrowdsMaster(rpyc.Service):
+    #called by client
+    def exposed_connect_me(self, ip, port):
+        global data
+        #client = rpyc.connect(ip, port, config={"allow_all_attrs": True})
+        #data.clientsRPYC.append(client)
+        data.clients.append((ip, port))
+
+    def exposed_get_clients(self):
+        global data
+        return data.clients
+
+    def exposed_disconnect_me(self, ip, port):
+        global data
+        data.clients.remove((ip, port))
+
+    #not using this anymore
+    def exposed_add_port(self, port):
+        global data
+        data.servers.append(port)
+
+class CrowdsIpChecker(rpyc.Service):
+    def exposed_check_ip(self, sIp):
+        global ipGiver
+		return ipGiver.chooseIp(sIp)
+
+def server_start():
+    ThreadedServer(CrowdsMaster, port=data.port,
+                   protocol_config={"allow_public_attrs": True, "allow_all_attrs": True}).start()
+
+def launch_Ip_checker()
+	ThreadedServer(CrowdsIpChecker,2222,protocol_config={"allow_public_attrs": True, "allow_all_attrs": True}).start()
+
+
+def main():
+    global data
+    data = PersistentData()
+	
+	ipGiver=IPv4(ip)
+	assignedIp=ipGiver.chooseIp()
+	thread.start_new_thread(launch_Ip_checker, ())
+	
+    thread.start_new_thread(server_start, ())
+    myConsole = CrowdsConsole()
+    myConsole.prompt = ">"
+    myConsole.cmdloop("Server has started\nAvailable commands\nstable_mathcing\nadd_internship\nremove_internship\n")
+
+if __name__ == "__main__":
+    main()
---
+++ IP/ipDbMethods.py
@@ -0,0 +1,320 @@
+import pymysql
+import thread
+
+import rpyc
+
+from ipv4 import IPv4
+
+
+class Client:
+    def __init__(self):
+        self.id = 0
+        self.userName = ""
+        self.experience = 0
+        self.duration = 0
+        self.password_encr = ""
+        self.password_salt = ""
+        self.locations = []
+        self.languages = []
+
+
+class Internship:
+    def __init__(self):
+        self.id = 0
+        self.company = 0
+        self.location = 0
+        self.language = 0
+        self.experience = 0
+        self.duration = 0
+
+    def __str__(self):
+        stri = str(self.id)+" "+str(self.company)+" +"+str(self.location)+" "+str(self.language)+" "+str(self.experience)+" "+str(self.duration)
+        return stri
+
+    def clone(self):
+        internship2 = Internship()
+        internship2.id = self.id
+        internship2.company = self.company
+        internship2.location = self.location
+        internship2.language = self.language
+        internship2.experience = self.experience
+        internship2.duration = self. duration
+        return internship2
+
+
+class Company:
+    def __init__(self):
+        self.id = 0
+        self.name = ""
+
+
+class Location:
+    def __init__(self):
+        self.id = 0
+        self.city = ""
+
+
+class Language:
+    def __init__(self):
+        self.id = 0
+        self.name = ""
+
+
+class DBConnectionService(rpyc.Service):
+    class exposed_DBConnection(object):
+        def __init__(self):
+            self.host = "localhost"
+            self.user = "root"
+            self.password = "10lase"
+            self.dbName = "internship"
+            self.connection = None
+            self.cursor = None
+
+        def connect(self):
+            self.connection = pymysql.connect(self.host, self.user, self.password, self.dbName)
+
+        def isConnected(self):
+            if self.connection is None:
+                return False
+            return True
+
+        def exposed_getClients(self):
+            self.connect()
+            self.cursor = self.connection.cursor()
+            clients = []
+            self.cursor.execute("SELECT * FROM Client")
+            resultSet = self.cursor.fetchall()
+            for row in resultSet:
+                client = Client()
+                client.id = row[0]
+                client.userName = row[1]
+                client.experience = row[4]
+                client.duration = row[5]
+                self.cursor.execute("SELECT * FROM Client_Language WHERE client_id=" + str(client.id))
+                resultSet2 = self.cursor.fetchall()
+                for e in resultSet2:
+                    client.languages.append(e[0])
+                self.cursor.execute("SELECT * FROM Client_Location WHERE client_id=" + str(client.id))
+                resultSet2 = self.cursor.fetchall()
+                for e in resultSet2:
+                    client.languages.append(e[1])
+                clients.append(client)
+            self.cursor.close()
+            self.connection.close()
+            return clients
+
+        def exposed_getInternships(self):
+            self.connect()
+            self.cursor = self.connection.cursor()
+            internships = []
+            self.cursor.execute("SELECT * FROM Internship")
+            resultSet = self.cursor.fetchall()
+            for row in resultSet:
+                internship = Internship()
+                internship.id = row[0]
+                internship.company = row[2]
+                internship.location = row[3]
+                internship.language = row[4]
+                internship.experience = row[5]
+                internship.duration = row[6]
+                internships.append(internship)
+            self.cursor.close()
+            self.connection.close()
+            return internships
+
+        def exposed_getCompanies(self):
+            self.connect()
+            self.cursor = self.connection.cursor()
+            companies = []
+            sql = "SELECT * FROM Company"
+            self.cursor.execute(sql)
+            resultSet = self.cursor.fetchall()
+            for row in resultSet:
+                company = Company()
+                company.id = row[0]
+                company.name = row[1]
+                companies.append(company)
+            self.cursor.close()
+            self.connection.close()
+            return companies
+
+        def exposed_getLocations(self):
+            self.connect()
+            self.cursor = self.connection.cursor()
+            locations = []
+            sql = "SELECT * FROM Location"
+            self.cursor.execute(sql)
+            resultSet = self.cursor.fetchall()
+            for row in resultSet:
+                location = Location()
+                location.id = row[0]
+                location.city = row[1]
+                locations.append(location)
+            self.cursor.close()
+            self.connection.close()
+            return locations
+
+        def exposed_getLanguages(self):
+            self.connect()
+            self.cursor = self.connection.cursor()
+            languages = []
+            sql = "SELECT * FROM Language"
+            self.cursor.execute(sql)
+            resultSet = self.cursor.fetchall()
+            for row in resultSet:
+                language = Language()
+                language.id = row[0]
+                language.name = row[1]
+                languages.append(language)
+            self.cursor.close()
+            self.connection.close()
+            return languages
+
+        def exposed_insertClient(self, client):
+            self.connect()
+            self.cursor = self.connection.cursor()
+            sql = """INSERT INTO Clieant(client_id, username, password_encr, password_salt, 
+            experience, duration) VALUES ('%d','%s','%s','%s','%d','%d')""" % \
+                  (client.id, client.userName, client.password_encr, client.password_salt, \
+                   client.experience, client.duration)
+            self.cursor.execute(sql)
+            for l in client.languages:
+                sql = """INSERT INTO Client_Language(language_id, client_id)
+                VALUES ('%d', '%d')""" % (l, client.id)
+                self.cursor.execute(sql)
+            for l in client.locations:
+                sql = """INSERT INTO Client_Location(client_id, location_id)
+                VALUES ('%d', '%d')""" % (client.id, l)
+                self.cursor.execute(sql)
+            self.connection.commit()
+            self.cursor.close()
+            self.connection.close()
+
+        def exposed_insertInternship(self, internship):
+            self.connect()
+            self.cursor = self.connection.cursor()
+            sql = """INSERT INTO Internship (internship_id, company_id,
+            location_id, language_id, experience, duration) values ('%d', '%d',
+            '%d', '%d', '%d', '%d')""" % (internship.id, internship.company, internship.location, \
+                                          internship.language, internship.experience, internship.duration)
+            self.cursor.execute(sql)
+            self.connection.commit()
+            self.cursor.close()
+            self.connection.close()
+
+        def exposed_insertLanguage(self, language):
+            self.connect()
+            self.cursor = self.connection.cursor()
+            sql = """INSERT INTO Language (language_id, name)
+            values ('%d', '%s')""" % (language.id, language.name)
+            self.cursor.execute(sql)
+            self.connection.commit()
+            self.cursor.close()
+            self.connection.close()
+
+        def exposed_insertLocation(self, location):
+            self.connect()
+            self.cursor = self.connection.cursor()
+            sql = """INSERT INTO Location (location_id, city)
+            values ('%d', '%s')""" % (location.id, location.city)
+            self.cursor.execute(sql)
+            self.connection.commit()
+            self.cursor.close()
+            self.connection.close()
+
+        def exposed_insertCompany(self, company):
+            self.connect()
+            self.cursor = self.connection.cursor()
+            sql = """INSERT INTO Company (company_id, name)
+            values ('%d', '%s')""" % (company.id, company.name)
+            self.cursor.execute(sql)
+            self.connection.commit()
+            self.cursor.close()
+            self.connection.close()
+
+        def exposed_deleteClient(self, client):
+            self.connect()
+            self.cursor = self.connection.cursor()
+            sql = "DELETE FROM Client_Location WHERE client_id='%d'" % (client.id)
+            self.cursor.execute(sql)
+            sql = "DELETE FROM Client_Language WHERE client_id='%d'" % (client.id)
+            self.cursor.execute(sql)
+            sql = "DELETE FROM Client WHERE client_id='%d'" % (client.id)
+            self.cursor.execute(sql)
+            self.connection.commit()
+            self.cursor.close()
+            self.connection.close()
+
+        def exposed_deleteInternship(self, internship):
+            self.connect()
+            self.cursor = self.connection.cursor()
+            sql = "Delete FROM Internship where internship_id = '%d'" % internship.id
+            self.cursor.execute(sql)
+            self.connection.commit()
+            self.cursor.close()
+            self.connection.close()
+
+        def exposed_deleteLanguage(self, language):
+            self.connect()
+            self.cursor = self.connection.cursor()
+            sql = "DELETE FROM Internship WHERE language_id='%d'" % (language.id)
+            self.cursor.execute(sql)
+            sql = "DELETE FROM Client_Language WHERE language_id='%d'" % (language.id)
+            self.cursor.execute(sql)
+            sql = "DELETE FROM Language WHERE language_id='%d'" % (language.id)
+            self.cursor.execute(sql)
+            self.connection.commit()
+            self.cursor.close()
+            self.connection.close()
+
+        def exposed_deleteLocation(self, location):
+            self.connect()
+            self.cursor = self.connection.cursor()
+            sql = "DELETE FROM Client_Location WHERE location_id='%d'" % (location.id)
+            self.cursor.execute(sql)
+            sql = "DELETE FROM Internship WHERE location_id='%d'" % (location.id)
+            self.cursor.execute(sql)
+            sql = "DELETE FROM Location WHERE location_id='%d'" % (location.id)
+            self.cursor.execute(sql)
+            self.connection.commit()
+            self.cursor.close()
+            self.connection.close()
+
+        def exposed_deleteCompany(self, company):
+            self.connect()
+            self.cursor = self.connection.cursor()
+            sql = "DELETE FROM Internship WHERE company_id='%d'" % (company.id)
+            self.cursor.execute(sql)
+            sql = "DELETE FROM Company WHERE company_id='%d'" % (company.id)
+            self.cursor.execute(sql)
+            self.connection.commit()
+            self.cursor.close()
+            self.connection.close()
+
+        def exposed_Sal(self):
+            print("PaBafta")
+
+class DBIpChecker(rpyc.Service):
+    def exposed_check_ip(self, sIp):
+        global ipGiver
+		return ipGiver.chooseIp(sIp)
+		
+def server_start():
+    ThreadedServer(DBConnectionService, port=1234,protocol_config={"allow_public_attrs": True, "allow_all_attrs": True}).start()
+
+def launch_Ip_checker()
+	ThreadedServer(DBIpChecker,2222,protocol_config={"allow_public_attrs": True, "allow_all_attrs": True}).start()
+
+from rpyc.utils.server import ThreadedServer
+
+ipGiver=IPv4(ip)
+assignedIp=ipGiver.chooseIp()
+thread.start_new_thread(launch_Ip_checker, ())
+
+if __name__ == "__main__":
+    print "ok"
+    thread.start_new_thread(server_start,())
+    print "also ok"
+	print "My assignedIp is" + assignedIp
+    while 1==1:
+        pass
